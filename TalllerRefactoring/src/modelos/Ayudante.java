2320dd0a463b999db0e0e9ad96d9a8353ec60400
Josue Tomala <jotopozo@espol.edu.ec>
2020-08-13 13:06:22 -0500

    Fix_Clase_Ayudante

--- TalllerRefactoring/src/modelos/Ayudante.java
+++ TalllerRefactoring/src/modelos/Ayudante.java
@@ -2,36 +2,23 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Ayudante {
-    protected Estudiante est;
-    public ArrayList<Paralelo> paralelos;
-
-    Ayudante(Estudiante e){
-    	est = e;
-    }
-    public String getMatricula() {
-        return est.getMatricula();
+public class Ayudante extends Estudiante{
+    public ArrayList<Paralelo> ayudantias;
+    Ayudante(){
+    	super();
     }
 
-    public void setMatricula(String matricula) {
-        est.setMatricula(matricula);
+    public ArrayList<Paralelo> getAyudantias() {
+        return ayudantias;
     }
 
-    //Getters y setters se delegan en objeto estudiante para no duplicar código
-    public String getNombre() {
-        return est.getNombre();
+    public void setAyudantias(ArrayList<Paralelo> ayudantias) {
+        this.ayudantias = ayudantias;
     }
-
-    public String getApellido() {
-        return est.getApellido();
-    }
-
-    //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
-
-
+    
     //Método para imprimir los paralelos que tiene asignados como ayudante
     public void MostrarParalelos(){
-        for(Paralelo par:paralelos){
+        for(Paralelo par:ayudantias){
             //Muestra la info general de cada paralelo
         }
     }
